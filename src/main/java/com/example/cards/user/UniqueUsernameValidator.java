6563901c5e8677d8705709cc85bd1023fc6698a5
zheng.kai <zheng.kai@jyintgroup.com>
2024-02-21 18:09:08 +0800

    ConstraintViolationImpl{interpolatedMessage='此信箱已被使用，請換一組', propertyPath=email, rootBeanClass=class com.example.cards.user.UserModel, messageTemplate='此信箱已被使用，請換一組'} ConstraintViolationImpl{interpolatedMessage='此帳號已被使用，請換一組', propertyPath=username, rootBeanClass=class com.example.cards.user.UserModel, messageTemplate='此帳號已被使用，請換一組'} ] javax.validation.ConstraintViolationException: Validation failed for classes [com.example.cards.user.UserModel] during persist time for groups [javax.validation.groups.Default, ] List of constraint violations:[ 	ConstraintViolationImpl{interpolatedMessage='此信箱已被使用，請換一組', propertyPath=email, rootBeanClass=class com.example.cards.user.UserModel, messageTemplate='此信箱已被使用，請換一組'} 	ConstraintViolationImpl{interpolatedMessage='此帳號已被使用，請換一組', propertyPath=username, rootBeanClass=class com.example.cards.user.UserModel, messageTemplate='此帳號已被使用，請換一組'} ] 要解決這個問題

--- src/main/java/com/example/cards/user/UniqueUsernameValidator.java
+++ src/main/java/com/example/cards/user/UniqueUsernameValidator.java
@@ -5,14 +5,21 @@ import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class UniqueUsernameValidator implements ConstraintValidator<UniqueUsername, String> {
+    private static final Logger logger = LoggerFactory.getLogger(UniqueUsernameValidator.class);
+
     @Autowired
     private UserService userService;
 
-    @Override
     public boolean isValid(String username, ConstraintValidatorContext context) {
+        if (userService == null) {
+            logger.error("UserService is null. Injection failed.");
+            return false;
+        }
         return (userService.findUserByUsername(username) == null);
     }
 }
